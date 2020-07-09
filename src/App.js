3e2e0b4855c89146621d490f3d0a429d79b1d670
engrbugs <engrbugs@gmail.com>
2020-07-08 23:09:40 -0700

    2020-07-08-2309

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import marked from 'marked';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const initialMarkdown = `
-
-# Welcome to my React Markdown Previewer!
+const initialMarkdown =
+`# Welcome to my React Markdown Previewer!
+## by ENGRBUGS
 
 ## This is a sub-heading...
 ### And here's some other cool stuff:
@@ -49,56 +50,6 @@ And here. | Okay. | I think we get it.
 - Even if you use dashes or asterisks.
 * And last but not least, let's not forget embedded images:
 
-![React Logo w/ Text](https://goo.gl/Umyytc)
-
-Marked - Markdown Parser
-========================
-inline code, a code block, a list item, a blockquote
-[Marked] lets you convert [Markdown] into HTML.  Markdown is a simple text format whose goal is to be very easy to read and write, even when not converted to HTML.  This demo page will let you type anything you like and see how it gets converted.  Live.  No more waiting around.
-
-How To Use The Demo
--------------------
-
-# 1. Type in stuff on the left.
-## 2. See the live updates on the right.
-
-this is a inline \`<div>Hello, World!<br></div>\`
-
-block of code:
-
-\`\`\`
-  for (let i=0; range(5) i++) {
-    console.log("Hello World");
-  }
-  
-\`\`\`
-
-That's it.  Pretty simple.  There's also a drop-down option in the upper right to switch between various views:
-
-- **Preview:**  A live display of the generated HTML as it would render in a browser.
-- **HTML Source:**  The generated HTML before your browser makes it pretty.
-- **Lexer Data:**  What [marked] uses internally, in case you like gory stuff like this.
-- **Quick Reference:**  A brief run-down of how to format things using markdown.
-
-Why Markdown?
--------------
-
-It's easy.  It's not overly bloated, unlike HTML.  Also, as the creator of [markdown] says,
-
-> The overriding design goal for Markdown's
-> formatting syntax is to make it as readable
-> as possible. The idea is that a
-> Markdown-formatted document should be
-> publishable as-is, as plain text, without
-> looking like it's been marked up with tags
-> or formatting instructions.
-
-Ready to start writing?  Either start changing stuff on the left or
-[clear everything](/demo/?text=) with a simple click.
-
-[Marked]: https://github.com/markedjs/marked/
-[Markdown]: http://daringfireball.net/projects/markdown/
-
 ![freeCodeCamp](https://upload.wikimedia.org/wikipedia/commons/3/39/FreeCodeCamp_logo.png)
 `
 
@@ -112,35 +63,36 @@ class App extends React.Component {
   handleChange = (e) => {
     this.setState({
       text: e.target.value
-
     })
-    console.log(this.state.text);
   }
 
   render() {
     let { text } = this.state;
     let markdown = marked(text, { breaks: true });
     return (
-      <div className="App">
-        <div className=".App-header container-fluid">
-          <h1>Convert your Markdown</h1>
+        
+        <div className="App container-fluid h-100">
+        <div className="App-header row">
+          <h1>Markdown Editor</h1>
         </div>
         <div className="main row">
-          <div className="container">
-            <div className="">
-              <h6>Enter your markdown:</h6>
-              <textarea id="editor" className="form-control p-2" value={text} onChange={this.handleChange} />
-            </div>
+          <div className="col pr-2">
+              <div className="row" style={{ height: "40px" }}>
+              <h5 className="ml-3" style={{ marginTop: "12px" }}>Input</h5>
+              <button className="btn btn-clear btn-outline-info btn-sm">Clear</button>
+              </div>
+              <textarea id="editor" className="form-control" value={text} onChange={this.handleChange} />
           </div>
-          <div className="container">
-            <div className="">
-              <h6>See the result:</h6>
-              <div id="preview" className="preview rounded p-2" dangerouslySetInnerHTML={{__html: markdown}} contenteditable>
-                </div>
+          <div className="col pl-2">
+          <div className="row" style={{ height: "40px" }}>
+          <h5 className="ml-3" style={{ marginTop: "12px" }}>Markdown</h5>
             </div>
+            <div id="preview" className="preview rounded" dangerouslySetInnerHTML={{__html: markdown}} contenteditable>
+                </div>
           </div>
         </div>
-      </div>
+        </div>
+        
     );
   }
 }
